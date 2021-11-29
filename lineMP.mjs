cd53640272216ab9471cfcdcb75155ea5a3dc21a
Rita Borges <rita.borges@live.com>
2021-11-29 00:25:23 +0000

    possibilidade de finitude

--- lineMP.mjs
+++ lineMP.mjs
@@ -1,48 +1,44 @@
 export function lineMP ( pontoA, pontoB){
-        let x0 = pontoA[0];
-        let y0 = pontoA[1];
-        let x1 = pontoB[0];
-        let y1 = pontoB[1];
         let simetrico = false;                      //Variavel auxiliar para verificar se existiu trocas de valores
         let declive = false;                        //dos pontos devido a estarem noutro octante que não o padrão
-        let dx = x1 - x0;                           //DeltaX
-        let dy = y1 - y0;                           //DeltaY
+        let dx = pontoB.x - pontoA.x;                           //DeltaX
+        let dy = pontoB.y - pontoA.y;                           //DeltaY
         let m = dx * dy;                            //Declive da reta
         if(m < 0){                                  //Verifica se declive é negativo
             simetrico = true;                       //Se for negativo atualiza valor de simetrico
             dy = -dy;                               //e troca valores de deltay e y0 e y1
-            y0 = -y0;                               //para valores simétricos
-            y1 = -y1;
+            pontoA.y = -pontoA.y;                               //para valores simétricos
+            pontoB.y = -pontoB.y;
         }
         if(Math.abs(dx)< Math.abs(dy)){             //Verifica se o valor (absoluto) de deltaX é infeior ao de deltaY
             declive = true;                         //Se for atualiza o valor de declive
-            let aux = x0;                           //e troca a ordem das coordenadas (x,y) para (y,x)
-            x0= y0;                                //de ambos os pontos
-            y0 = aux;
-            aux = x1;
-            x1 = y1;
-            y1 = aux;
+            let aux = pontoA.x;                           //e troca a ordem das coordenadas (x,y) para (y,x)
+            pontoA.x= pontoA.y;                                //de ambos os pontos
+            pontoA.y = aux;
+            aux = pontoB.x;
+            pontoB.x = pontoB.y;
+            pontoB.y = aux;
             aux = dx;                               //e dos delta também
             dx = dy;
             dy = aux;
         }
-        if(x0 > x1){                               //verifica se valor de x0 é superior ao de x1
-            let aux = x0;                           //Caso seja troca a ordem dos pontos
-            x0 = x1;
-            x1 = aux;
-            aux = y0;
-            y0 = y1;
-            y1 = aux;
-            dx = -dx;                               //e troca os deltas para os valores simétricos deles
+        if(pontoA.x > pontoB.x){                               //verifica se valor de x0 é superior ao de x1
+            let aux = pontoA.x;                                //Caso seja troca a ordem dos pontos
+            pontoA.x = pontoB.x;
+            pontoB.x = aux;
+            aux = pontoA.y;
+            pontoA.y = pontoB.y;
+            pontoB.y = aux;
+            dx = -dx;                                  //e troca os deltas para os valores simétricos deles
             dy = -dy;
         }
         let pontos = [];                            //Variável que guarda os pontos calculados
-        let y = y0;
+        let y = pontoA.y;
         let incrementoE = 2 * dy;                   //Incremento para E
         let incrementoNE = 2 * (dy -dx);            //Incremento para NE
         let d = 2 * dy - dx;                        //Valor de calculo base para os incrementos
-        for (let x = x0; x < x1; x++){               //Calcula os pontos
-            pontos.push([x,y]);
+        for (let x = pontoA.x; x < pontoB.x; x++){               //Calcula os pontos
+            pontos.push({x:x,y:y});
             if(d <= 0){
                 d += incrementoE;                   //Se d for negativo ou nulo incrementa d para E
             } else {
@@ -50,19 +46,19 @@ export function lineMP ( pontoA, pontoB){
                 d += incrementoNE;                  //e d incrementa para NE
             }
         }
-        pontos.push([x1,y1]);                       //Adiciona a ultima coordenada da extremidade da linha
-        for(let i = 0; i < pontos.length; i++){     //Percorre o arrei de pontos Para atualizar os valores com base nas variáveis auxiliares
-            let x = pontos[i][0];
-            let y = pontos[i][1];
+        pontos.push([pontoB.x,pontoB.y]);          //Adiciona a ultima coordenada da extremidade da linha
+        for(let i = 0; i < pontos.length; i++){    //Percorre o array de pontos Para atualizar os valores com base nas variáveis auxiliares
+            let p = pontos[i];
+            //let y = pontos[i][1];
             if(declive == true){                    //Verifica se existiu troca de coordenadas
-                let aux = x;                        //Caso exista volta a trocar x com y (calculados)
-                x = y;
-                y = aux;
+                let aux = p.x;                        //Caso exista volta a trocar x com y (calculados)
+                p.x = p.y;
+                p.y = aux;
             }
             if(simetrico == true){                  //Verifica se existiu troca de valores para simétricos
-                y = -y;                             //Caso exista volta a trocar y (calculado) para o seu simétrico
+                p.y = -p.y;                             //Caso exista volta a trocar y (calculado) para o seu simétrico
             }
-            pontos[i]= [x,y];                       //Atualiza os valores no array de pontos
+            pontos[i]= {x:p.x,y:p.y};                       //Atualiza os valores no array de pontos
         }
         return pontos;
 }
--- src/Interface.mjs
+++ src/Interface.mjs
@@ -4,7 +4,13 @@ import { OrbitControls } from 'https://unpkg.com/three@0.124.0/examples/jsm/cont
 let scene;
 let camera;
 let renderer;
-let controls
+let controls;
+let raycaster;
+let ultimoPixel;
+let pixel1=null;
+let pixel2=null;
+
+const mouse = new THREE.Vector3();
 
 export function Inicio (){
     setup();
@@ -12,7 +18,6 @@ export function Inicio (){
     animate();
     eixoPositivoX();
     eixoPositivoY();
-    cubosAmarelos (0,0,1);
 
 }
 
@@ -20,19 +25,24 @@ function setup (){
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 1,2000);
     renderer = new THREE.WebGLRenderer();
+    renderer.name="setup";
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
 
     const light = new THREE.AmbientLight( 0x404040 ); // soft white light
+    light.name="setup";
     scene.add( light );
 
     camera.position.x = 0;
     camera.position.y = 0;
     camera.position.z = 120;
+    camera.name="setup";
 
     controls = new OrbitControls( camera, renderer.domElement );
     controls.enableZoom = true;
     controls.autoRotate = false;
+
+    raycaster = new THREE.Raycaster()
 }
 
 function animate() {
@@ -40,45 +50,38 @@ function animate() {
     requestAnimationFrame( animate );
     renderer.render( scene, camera );
 }
-function desenharPlanoRosa(x,y){
+
+function desenharPlanoRosa(x,y, pos){
     const geometry = new THREE.PlaneGeometry(5, 5 );
     const material = new THREE.MeshBasicMaterial( {color: 0xED77C4, side: THREE.DoubleSide} );
     const plane = new THREE.Mesh( geometry, material );
     plane.position.x=x;
     plane.position.y=y;
     plane.position.z=0;
+    plane.name= pos;
     scene.add( plane );
 }
-function desenharPlanoAzul(x,y){
+function desenharPlanoAzul(x,y, pos){
     const geometry = new THREE.PlaneGeometry( 5, 5 );
     const material = new THREE.MeshBasicMaterial( {color: 0x5EB1BC, side: THREE.DoubleSide} );
     const plane = new THREE.Mesh( geometry, material );
     plane.position.x=x;
     plane.position.y=y;
     plane.position.z=0;
-    scene.add( plane );
-}
-
-function desenharPlano(x,y){
-    const geometry = new THREE.PlaneGeometry( 5, 5 );
-    const material = new THREE.MeshBasicMaterial( {color: 0xffffff, side: THREE.DoubleSide} );
-    const plane = new THREE.Mesh( geometry, material );
-    plane.position.x=x;
-    plane.position.y=y;
-    plane.position.z=0;
+    plane.name= pos;
     scene.add( plane );
 }
 
 function planos (){
-    for(let y=-105, a=0;a < 43;a++, y= y+5){
-        for (let i=-105,e=0; e < 43; e++, i=i+5){
+    for(let y=-105, a=0, py=-21;a < 43;a++, y= y+5, py++){
+        for (let i=-105,e=0, px=-21; e < 43; e++, i=i+5, px++){
+            let pos = "["+px + "," + py+"]"
             if ((a % 2 == 0 && e % 2 ==0)|| (a % 2 != 0) && (e % 2 != 0)){
-                desenharPlanoRosa(i,y);
+                desenharPlanoRosa(i,y, pos);
             }else
-                desenharPlanoAzul(i,y);
+                desenharPlanoAzul(i,y, pos);
             }
      }
-    desenharPlano(0,0)
 }
 
 function eixoPositivoX( ){
@@ -90,6 +93,7 @@ function eixoPositivoX( ){
 
     const geometry = new THREE.BufferGeometry().setFromPoints( points );
     const line = new THREE.Line( geometry, material );
+    line.name= "eixoX"
     scene.add( line );
     renderer.render( scene, camera );
 
@@ -104,8 +108,72 @@ function eixoPositivoY( ){
 
     const geometry = new THREE.BufferGeometry().setFromPoints( points );
     const line = new THREE.Line( geometry, material );
+    line.name="eixoY";
     scene.add( line );
     renderer.render( scene, camera );
+}
+
+function detectarObjeto() {
+    // update the picking ray with the camera and mouse position
+    raycaster.setFromCamera( mouse, camera );
+    // calculate objects intersecting the picking ray
+    const intersects = raycaster.intersectObjects( scene.children );
+    let objeto;
+    for ( let i = 0; i < intersects.length; i ++ ) {
+        objeto = intersects[i].object;
+        //if(objeto.name != ultimoPixel) {
+            if (objeto.name != "setup" && objeto.name != "eixoX" && objeto.name != "eixoY") {
+                return objeto;
+            }
+        //}
+    }
+    //renderer.render( scene, camera );
+    return null;
+}
+function selecionarPixel(objeto){
+    objeto.material.color.set( 0xff0000 );
+    let coord = objeto.name.split(",");
+    let x = coord[0].substring(1);
+    let y = coord[1].substring(0,coord[1].length);
+    let ponto = {x:x, y:y}
+    if (pixel1 == null){
+        pixel1= ponto;
+    }
+    else {
+        pixel2 = ponto;
+        cubosAmarelos(0,0,0)
+    }
+}
+function desenharLinha(){
+
+}
+
+function imprimirNome(objeto) {
+    if(objeto.name != ultimoPixel) {
+        console.log(objeto.name)
+        ultimoPixel = objeto.name;
+    }
+}
+window.addEventListener( 'mousemove', onMouseMove, false );
+function onMouseMove( event ) {
+    let objeto = detectarObjeto();
+    if (objeto) {
+        imprimirNome(objeto);
+    }
+    // calculate mouse position in normalized device coordinates
+    // (-1 to +1) for both components
+    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+}
+
+window.addEventListener( 'click', onMouseDown, false );
+function onMouseDown( event ) {
+    let objeto = detectarObjeto()
+    if(objeto){
+        selecionarPixel(objeto);
+    }
+    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
 }
 function cubosAmarelos (x,y,z){
     const geometry = new THREE.BoxGeometry( 5, 5, 5/4 );
@@ -118,5 +186,5 @@ function cubosAmarelos (x,y,z){
     cube.position.x=x;
     cube.position.y=y;
     cube.position.z=z;
-    cubo.name = "PixelAmarelo";
+    cube.name = "PixelAmarelo";
 }
