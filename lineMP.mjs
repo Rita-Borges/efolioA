889f70cbf8b81b0a496b98fe3c0e7d0f860d4eea
ritaborges <ryta.borgez@gmail.com>
2023-11-19 12:50:22 +0000

    new implementation

--- lineMP.mjs
+++ lineMP.mjs
@@ -1,64 +1,56 @@
-export function lineMP ( pontoA, pontoB){
-        let simetrico = false;                      //Variavel auxiliar para verificar se existiu trocas de valores
-        let declive = false;                        //dos pontos devido a estarem noutro octante que não o padrão
-        let dx = pontoB.x - pontoA.x;                           //DeltaX
-        let dy = pontoB.y - pontoA.y;                           //DeltaY
-        let m = dx * dy;                            //Declive da reta
-        if(m < 0){                                  //Verifica se declive é negativo
-            simetrico = true;                       //Se for negativo atualiza valor de simetrico
-            dy = -dy;                               //e troca valores de deltay e y0 e y1
-            pontoA.y = -pontoA.y;                               //para valores simétricos
-            pontoB.y = -pontoB.y;
+export function lineMP(pontoA, pontoB) {
+    let trocarCoordenadas = false;
+    let inverterValores = false;
+
+    let dx = pontoB.x - pontoA.x;
+    let dy = pontoB.y - pontoA.y;
+
+    // Verificar e ajustar simetria
+    if (dx * dy < 0) {
+        inverterValores = true;
+        dy = -dy;
+        pontoA.y = -pontoA.y;
+        pontoB.y = -pontoB.y;
+    }
+
+    // Verificar e ajustar declive
+    if (Math.abs(dx) < Math.abs(dy)) {
+        trocarCoordenadas = true;
+        [pontoA.x, pontoA.y, pontoB.x, pontoB.y, dx, dy] = [pontoA.y, pontoA.x, pontoB.y, pontoB.x, dy, dx];
+    }
+
+    // Garantir que pontoA é à esquerda de pontoB
+    if (pontoA.x > pontoB.x) {
+        [pontoA.x, pontoB.x, pontoA.y, pontoB.y, dx, dy] = [pontoB.x, pontoA.x, pontoB.y, pontoA.y, -dx, -dy];
+    }
+
+    let pontos = [];
+    let y = pontoA.y;
+    let incrementoE = 2 * dy;
+    let incrementoNE = 2 * (dy - dx);
+    let d = 2 * dy - dx;
+
+    for (let x = pontoA.x; x <= pontoB.x; x++) {
+        pontos.push({ x: x, y: y });
+
+        if (d <= 0) {
+            d += incrementoE;
+        } else {
+            y++;
+            d += incrementoNE;
         }
-        if(Math.abs(dx)< Math.abs(dy)){             //Verifica se o valor (absoluto) de deltaX é infeior ao de deltaY
-            declive = true;                         //Se for atualiza o valor de declive
-            let aux = pontoA.x;                           //e troca a ordem das coordenadas (x,y) para (y,x)
-            pontoA.x= pontoA.y;                                //de ambos os pontos
-            pontoA.y = aux;
-            aux = pontoB.x;
-            pontoB.x = pontoB.y;
-            pontoB.y = aux;
-            aux = dx;                               //e dos delta também
-            dx = dy;
-            dy = aux;
+    }
+
+    // Ajustar valores finais
+    pontos = pontos.map(p => {
+        if (trocarCoordenadas) {
+            [p.x, p.y] = [p.y, p.x];
         }
-        if(pontoA.x > pontoB.x){                               //verifica se valor de x0 é superior ao de x1
-            let aux = pontoA.x;                                //Caso seja troca a ordem dos pontos
-            pontoA.x = pontoB.x;
-            pontoB.x = aux;
-            aux = pontoA.y;
-            pontoA.y = pontoB.y;
-            pontoB.y = aux;
-            dx = -dx;                                  //e troca os deltas para os valores simétricos deles
-            dy = -dy;
+        if (inverterValores) {
+            p.y = -p.y;
         }
-        let pontos = [];                            //Variável que guarda os pontos calculados
-        let y = pontoA.y;
-        let incrementoE = 2 * dy;                   //Incremento para E
-        let incrementoNE = 2 * (dy -dx);            //Incremento para NE
-        let d = 2 * dy - dx;                        //Valor de calculo base para os incrementos
-        for (let x = pontoA.x; x < pontoB.x; x++){               //Calcula os pontos
-            pontos.push({x:x,y:y});
-            if(d <= 0){
-                d += incrementoE;                   //Se d for negativo ou nulo incrementa d para E
-            } else {
-                y++;                                //Se d for positivo incrementa y
-                d += incrementoNE;                  //e d incrementa para NE
-            }
-        }
-        pontos.push([pontoB.x,pontoB.y]);          //Adiciona a ultima coordenada da extremidade da linha
-        for(let i = 0; i < pontos.length; i++){    //Percorre o array de pontos Para atualizar os valores com base nas variáveis auxiliares
-            let p = pontos[i];
-            //let y = pontos[i][1];
-            if(declive == true){                    //Verifica se existiu troca de coordenadas
-                let aux = p.x;                        //Caso exista volta a trocar x com y (calculados)
-                p.x = p.y;
-                p.y = aux;
-            }
-            if(simetrico == true){                  //Verifica se existiu troca de valores para simétricos
-                p.y = -p.y;                             //Caso exista volta a trocar y (calculado) para o seu simétrico
-            }
-            pontos[i]= {x:p.x,y:p.y};                       //Atualiza os valores no array de pontos
-        }
-        return pontos;
+        return { x: p.x, y: p.y };
+    });
+
+    return pontos;
 }
--- src/Interface.mjs
+++ src/Interface.mjs
@@ -1,18 +1,32 @@
 import * as THREE from 'https://unpkg.com/three@0.124.0/build/three.module.js';
-import { OrbitControls } from 'https://unpkg.com/three@0.124.0/examples/jsm/controls/OrbitControls.js'
+import {OrbitControls} from 'https://unpkg.com/three@0.124.0/examples/jsm/controls/OrbitControls.js'
+import {lineMP} from "../lineMP.mjs";
 
-let scene;
+
+let scene = new THREE.Scene();
 let camera;
 let renderer;
 let controls;
 let raycaster;
 let ultimoPixel;
-let pixel1=null;
-let pixel2=null;
+let linhaExata;
+let pixel1 = null;
+let pixel2 = null;
+let objPixel1 = null;
+let objPixel2 = null;
+let obj1Cor = null;
+let obj2Cor = null;
+
+let cameraStartPosition = new THREE.Vector3(0, 0, 120);
+let cameraTopViewPosition = new THREE.Vector3(0, 0, 300);
+let cameraAnimationDuration = 1000;  // Duração da animação em milissegundos
+let cameraAnimationStartTime = null;
+
 
 const mouse = new THREE.Vector3();
 
-export function Inicio (){
+
+export function iniciarInterface() {
     setup();
     planos();
     animate();
@@ -21,24 +35,23 @@ export function Inicio (){
 
 }
 
-function setup (){
-    scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 1,2000);
+function setup() {
+    camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 1, 2000);
     renderer = new THREE.WebGLRenderer();
-    renderer.name="setup";
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
+    renderer.name = "setup";
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
 
-    const light = new THREE.AmbientLight( 0x404040 ); // soft white light
-    light.name="setup";
-    scene.add( light );
+    const light = new THREE.AmbientLight(0x404040); // soft white light
+    light.name = "setup";
+    scene.add(light);
 
     camera.position.x = 0;
     camera.position.y = 0;
     camera.position.z = 120;
-    camera.name="setup";
+    camera.name = "setup";
 
-    controls = new OrbitControls( camera, renderer.domElement );
+    controls = new OrbitControls(camera, renderer.domElement);
     controls.enableZoom = true;
     controls.autoRotate = false;
 
@@ -47,144 +60,248 @@ function setup (){
 
 function animate() {
     controls.update();
-    requestAnimationFrame( animate );
-    renderer.render( scene, camera );
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
 }
 
-function desenharPlanoRosa(x,y, pos){
-    const geometry = new THREE.PlaneGeometry(5, 5 );
-    const material = new THREE.MeshBasicMaterial( {color: 0xED77C4, side: THREE.DoubleSide} );
-    const plane = new THREE.Mesh( geometry, material );
-    plane.position.x=x;
-    plane.position.y=y;
-    plane.position.z=0;
-    plane.name= pos;
-    scene.add( plane );
-}
-function desenharPlanoAzul(x,y, pos){
-    const geometry = new THREE.PlaneGeometry( 5, 5 );
-    const material = new THREE.MeshBasicMaterial( {color: 0x5EB1BC, side: THREE.DoubleSide} );
-    const plane = new THREE.Mesh( geometry, material );
-    plane.position.x=x;
-    plane.position.y=y;
-    plane.position.z=0;
-    plane.name= pos;
-    scene.add( plane );
+function desenharPlanoRosaEscuro(x, y, pos) {
+    const geometry = new THREE.PlaneGeometry(5, 5);
+    const material = new THREE.MeshBasicMaterial({color: 0xDDA0DD, side: THREE.DoubleSide});
+    let plane = new THREE.Mesh(geometry, material);
+    plane.position.x = x;
+    plane.position.y = y;
+    plane.position.z = 0;
+    plane.name = pos;
+    scene.add(plane);
 }
 
-function planos (){
-    for(let y=-105, a=0, py=-21;a < 43;a++, y= y+5, py++){
-        for (let i=-105,e=0, px=-21; e < 43; e++, i=i+5, px++){
-            let pos = "["+px + "," + py+"]"
-            if ((a % 2 == 0 && e % 2 ==0)|| (a % 2 != 0) && (e % 2 != 0)){
-                desenharPlanoRosa(i,y, pos);
-            }else
-                desenharPlanoAzul(i,y, pos);
-            }
-     }
+function desenharPlanoRosaClaro(x, y, pos) {
+    const geometry = new THREE.PlaneGeometry(5, 5);
+    const material = new THREE.MeshBasicMaterial({color: 0xFF69B4, side: THREE.DoubleSide});
+    const plane = new THREE.Mesh(geometry, material);
+    plane.position.x = x;
+    plane.position.y = y;
+    plane.position.z = 0;
+    plane.name = pos;
+    scene.add(plane);
 }
 
-function eixoPositivoX( ){
+function planos() {
+    for (let y = -105, a = 0, py = -21; a < 43; a++, y = y + 5, py++) {
+        for (let i = -105, e = 0, px = -21; e < 43; e++, i = i + 5, px++) {
+            let pos = "[" + px + "," + py + "]"
+            if ((a % 2 === 0 && e % 2 === 0) || (a % 2 !== 0) && (e % 2 !== 0)) {
+                desenharPlanoRosaEscuro(i, y, pos);
+            } else
+                desenharPlanoRosaClaro(i, y, pos);
+        }
+    }
+}
+
+function eixoPositivoX() {
     //create a blue LineBasicMaterial
-    const material = new THREE.LineBasicMaterial( { color: 0x242291 } );
+    const material = new THREE.LineBasicMaterial({color: 0x242291});
     const points = [];
-    points.push( new THREE.Vector3( 0, 0, 0 ) );
-    points.push( new THREE.Vector3( 107, 0, 0 ) );
+    points.push(new THREE.Vector3(0, 0, 1));
+    points.push(new THREE.Vector3(107, 0, 1));
 
-    const geometry = new THREE.BufferGeometry().setFromPoints( points );
-    const line = new THREE.Line( geometry, material );
-    line.name= "eixoX"
-    scene.add( line );
-    renderer.render( scene, camera );
+    const geometry = new THREE.BufferGeometry().setFromPoints(points);
+    const line = new THREE.Line(geometry, material);
+    line.name = "eixoX"
+    scene.add(line);
+    renderer.render(scene, camera);
 
 }
 
-function eixoPositivoY( ){
+function eixoPositivoY() {
     //create a blue LineBasicMaterial
-    const material = new THREE.LineBasicMaterial( { color: 0XC70845} );
+    const material = new THREE.LineBasicMaterial({color: 0XC70845});
     const points = [];
-    points.push( new THREE.Vector3( 0, 0, 0 ) );
-    points.push( new THREE.Vector3( 0, 107, 0 ) );
+    points.push(new THREE.Vector3(0, 0, 1));
+    points.push(new THREE.Vector3(0, 107, 1));
 
-    const geometry = new THREE.BufferGeometry().setFromPoints( points );
-    const line = new THREE.Line( geometry, material );
-    line.name="eixoY";
-    scene.add( line );
-    renderer.render( scene, camera );
+    const geometry = new THREE.BufferGeometry().setFromPoints(points);
+    const line = new THREE.Line(geometry, material);
+    line.name = "eixoY";
+    scene.add(line);
+    renderer.render(scene, camera);
 }
 
 function detectarObjeto() {
     // update the picking ray with the camera and mouse position
-    raycaster.setFromCamera( mouse, camera );
+    raycaster.setFromCamera(mouse, camera);
     // calculate objects intersecting the picking ray
-    const intersects = raycaster.intersectObjects( scene.children );
+    const intersects = raycaster.intersectObjects(scene.children);
     let objeto;
-    for ( let i = 0; i < intersects.length; i ++ ) {
+    for (let i = 0; i < intersects.length; i++) {
         objeto = intersects[i].object;
-        //if(objeto.name != ultimoPixel) {
-            if (objeto.name != "setup" && objeto.name != "eixoX" && objeto.name != "eixoY") {
-                return objeto;
-            }
-        //}
+        if (objeto.name !== "setup" && objeto.name !== "eixoX" && objeto.name !== "eixoY" && objeto.name !== "PixelAmarelo") {
+            return objeto;
+        }
     }
     //renderer.render( scene, camera );
     return null;
 }
-function selecionarPixel(objeto){
-    objeto.material.color.set( 0xff0000 );
+
+function selecionarPixel(objeto) {
+
     let coord = objeto.name.split(",");
-    let x = coord[0].substring(1);
-    let y = coord[1].substring(0,coord[1].length);
-    let ponto = {x:x, y:y}
-    if (pixel1 == null){
-        pixel1= ponto;
-    }
-    else {
+    coord = coord.map(coordenada => coordenada.replaceAll('[', '').replaceAll(']', ''))
+    let x = parseInt(coord[0])//.substring(1));
+    let y = parseInt(coord[1])//.substring(0,coord[1].length));
+    let ponto = {x: x, y: y}
+    if (pixel1 == null) {
+        pixel1 = ponto;
+        objPixel1 = objeto;
+        obj1Cor = objeto.material.color.getHex();
+        objeto.material.color.set(0xff0000);
+    } else {
         pixel2 = ponto;
-        cubosAmarelos(0,0,0)
+        objPixel2 = objeto;
+        obj2Cor = objeto.material.color.getHex();
+        objeto.material.color.set(0xff0000);
+        desenharLinha();
+        objPixel1.material.color.setHex(obj1Cor)
+        objPixel2.material.color.setHex(obj2Cor)
+        pixel1 = null;
+        pixel2 = null;
+        objPixel1 = null;
+        objPixel2 = null;
+        obj1Cor = null;
+        obj2Cor = null;
     }
 }
-function desenharLinha(){
 
+function desenharLinha() {
+    let pontosp = lineMP(pixel1, pixel2);
+    // Criar a linha exata preta
+    const materialLinha = new THREE.LineBasicMaterial({color: 0x000000});
+    const pointsLinha = [];
+    for (let i = 0; i < pontosp.length; i++) {
+        pointsLinha.push(new THREE.Vector3(pontosp[i].x * 5, pontosp[i].y * 5, 1));
+    }
+    const geometryLinha = new THREE.BufferGeometry().setFromPoints(pointsLinha);
+    linhaExata = new THREE.Line(geometryLinha, materialLinha);
+    linhaExata.name = 'linha';
+    scene.add(linhaExata);
+
+    // Criar os ladrilhos amarelos semi-transparentes
+    for (let i = 0; i < pontosp.length; i++) {
+        let amarelo = pontosp[i];
+        gerarCubosAmarelos(amarelo.x * 5, amarelo.y * 5, 1);
+    }
 }
 
 function imprimirNome(objeto) {
-    if(objeto.name != ultimoPixel) {
+    if (objeto.name !== ultimoPixel) {
         console.log(objeto.name)
         ultimoPixel = objeto.name;
     }
 }
-window.addEventListener( 'mousemove', onMouseMove, false );
-function onMouseMove( event ) {
+
+window.addEventListener('mousemove', onMouseMove, false);
+
+function onMouseMove(event) {
     let objeto = detectarObjeto();
     if (objeto) {
         imprimirNome(objeto);
     }
     // calculate mouse position in normalized device coordinates
     // (-1 to +1) for both components
-    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
 }
 
-window.addEventListener( 'click', onMouseDown, false );
-function onMouseDown( event ) {
+window.addEventListener('click', onMouseDown, false);
+
+function onMouseDown(event) {
     let objeto = detectarObjeto()
-    if(objeto){
+    if (objeto) {
         selecionarPixel(objeto);
     }
-    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
 }
-function cubosAmarelos (x,y,z){
-    const geometry = new THREE.BoxGeometry( 5, 5, 5/4 );
-    const material = new THREE.MeshBasicMaterial( {
+
+function gerarCubosAmarelos(x, y, z) {
+    const geometry = new THREE.BoxGeometry(5, 5, 5 / 4);
+    const material = new THREE.MeshBasicMaterial({
         color: 0xE9E207,
-        opacity: 0.5
-    } );
-    const cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
-    cube.position.x=x;
-    cube.position.y=y;
-    cube.position.z=z;
+        opacity: 0.5,
+        transparent: 0.8
+    });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+    cube.position.x = x;
+    cube.position.y = y;
+    cube.position.z = z;
     cube.name = "PixelAmarelo";
+}
+
+document.addEventListener('keydown', onkeypress, false);
+
+function onkeypress(event) {
+    //Função para aceitar os eventos da tecla para mudar a vista
+
+    let key = event.which;
+    switch (key) {
+        case 8:                                                                                    //Verifica Tecla "backspace"
+            limparV();                                                                                 //Altera para a vista alternativa
+            break;
+
+        case 88:                                                                                    //Verifica Tecla "x"
+            selecionarPixel(detectarObjeto())//Lê caixa de texto e desenha pontos
+            break;
+        case 67:  // Tecla "C"
+            moverCameraTopView();
+            break;
+
+    }
+}
+
+function moverCameraTopView() {
+    controls.enabled = false;  // Desabilitar o controle durante a animação
+
+    cameraAnimationStartTime = performance.now();
+
+    function animateCamera() {
+        const currentTime = performance.now();
+        const elapsed = currentTime - cameraAnimationStartTime;
+
+        if (elapsed < cameraAnimationDuration) {
+            const t = elapsed / cameraAnimationDuration;
+            camera.position.lerpVectors(cameraStartPosition, cameraTopViewPosition, t);
+            requestAnimationFrame(animateCamera);
+        } else {
+            camera.position.copy(cameraTopViewPosition);
+            controls.enabled = true;  // Animacao concluída, permitir controle novamente
+        }
+
+        renderer.render(scene, camera);
+    }
+
+    animateCamera();
+}
+
+function limparV() {
+    let obj;
+    do {
+        obj = scene.getObjectByName("PixelAmarelo");
+        if (obj) {
+            scene.remove(obj);
+            obj.material.dispose();
+        }
+    } while (obj);
+    // Remover a linha preta
+    scene.remove(linhaExata);
+    do {
+        obj = scene.getObjectByName("linha");
+        if (obj) {
+            scene.remove(obj);
+            obj.material.dispose();
+        }
+    } while (obj);
+    renderer.render(scene, camera);
+
+
 }
